81638bc084bc48ef8be81e0df9c139cfb37a976e
Shehroaz Aslam <shehroaz.aslam@allente.tv>
2022-08-21 22:39:52 +0500

    Initial Spring Boot commit

---
+++ src/main/java/com/sherry/employeeangularcrud/EmployeeAngularCrudApplication.java
@@ -0,0 +1,13 @@
+package com.sherry.employeeangularcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAngularCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAngularCrudApplication.class, args);
+	}
+
+}
